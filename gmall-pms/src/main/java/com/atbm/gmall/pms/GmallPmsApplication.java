bd437821a8a477e82d4dbf5049defb4a58e159eb
BM_风华霜落 <1663005774@qq.com>
2020-01-23 00:51:57 +0800

    项目逆向完成...

--- gmall-pms/src/main/java/com/atbm/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atbm/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,21 @@
 package com.atbm.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+* 1、配置dubbo
+* 2、mybatis plus
+*   logstash整合
+*   1、依赖
+*   2、日志配置文件
+*   3、在kibana建立日志索引
+* */
+@EnableDubbo
+@MapperScan(basePackages = "com.atbm.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
